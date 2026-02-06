bce5e549118ac3f8efbe54669045bcb76b53cb68
NOel BEnny <169208982+noelbenny111@users.noreply.github.com>
2026-02-06 13:35:07 +0100

    called the kiethely command using with (open and close)

--- Exp_1.py
+++ Exp_1.py
@@ -5,37 +5,34 @@ from measurement_control.instruments.keithley_2600 import Keithley2600
 from measurement_control.communicators import GPIBCommunicator
 from measurement_control.enums import SMU, Range
 from measurement_control.utils.sweep_lists import create_sweep_list
-from measurements.memristor import MemristorMeasurements
+from memristor_meas import Memristor
 
 # -------------------------------------------------
 # 1. Connect to Keithley
 # -------------------------------------------------
-k = GPIBCommunicator(26, visa_library='@py')
 
 
-# -------------------------------------------------
-# 2. Create memristor measurement interface
-# -------------------------------------------------
-mem = MemristorMeasurements(k)
 
+with Keithley2600(GPIBCommunicator(26, visa_library='@py')) as k2600:
+    
+    # 2. Pass the active instrument instance to your Memristor class
+    mem = Memristor(k2600)
 
-ds = mem.bipolar_memristor_sweep(
-    set_list=create_sweep_list((5,), 0.1),
-    reset_list=create_sweep_list((-5,), 0.1),
-    settling_time=0.01,
-    set_compliance=0.0001,
-    reset_compliance=0.01,
-    voltage_range=5,
-    current_range=Range.AUTO
-)
+    # 3. Perform the sweep inside the block
+    ds = mem.bipolar_memristor_sweep(
+        set_list=create_sweep_list((5,), 0.1),
+        reset_list=create_sweep_list((-5,), 0.1),
+        settling_time=0.01,
+        set_compliance=0.0001,
+        reset_compliance=0.01,
+        voltage_range=5,
+        current_range=Range.AUTO
+    )
 
-
-print(ds.metadata)
-print(ds.data.head())
+    # The connection is active here
+    print("Sweep complete. Data head:")
+    print(ds.data.head())
 
 
 # Optional: save
 ds.data.to_csv("bipolar_memristor_sweep.txt", index=False)
-
-
-k.close()
